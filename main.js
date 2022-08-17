f195185d47cdf289a8f9b39ae24042a9892f4311
naman0123 <83550704+naman0123@users.noreply.github.com>
2022-08-17 18:03:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+
+    circle(150, 150, 54);
+    ellipse(60, 70, 40, 76);
+    rect(205, 230, 60, 50);
+}
+
+function draw() {
+}
